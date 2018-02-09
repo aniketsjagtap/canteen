8c341bc4191c58b6ddea1f1c4d6466b756b6011c
aniketsjagtap <aniketsjagtap@gmail.com>
2018-02-09 11:17:48 +0530

    add button to get data

--- views/sale/getData.php
+++ views/sale/getData.php
@@ -20,6 +20,10 @@ $(document).ready(function(){
       	<div class="box box-info">
             <div class="box-header with-border">
               	<h3 class="box-title">Sales</h3>
+				<div class="box-tools">
+                    <a href="<?php echo site_url('sale/add');?>" class="btn btn-success btn-sm">Add</a> 
+					
+                </div>
             </div>
 			 
             <?php echo form_open('Sale/getData'); ?>
